2114f755d216509dcc448e83a2a974da3f1f9185
Yazmin Carrillo <ycarrillo@csumb.edu>
2020-02-09 21:25:39 -0800

    Adding Part 2

---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by Yazmin Carrillo on 2/9/20.
+//  Copyright © 2020 Yazmin Carrillo. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
---
+++ flix/MovieGridDetailsViewController.swift
@@ -0,0 +1,50 @@
+//
+//  MovieGridDetailsViewController.swift
+//  flix
+//
+//  Created by Yazmin Carrillo on 2/9/20.
+//  Copyright © 2020 Yazmin Carrillo. All rights reserved.
+//
+
+import UIKit
+import AlamofireImage
+
+class MovieGridDetailsViewController: UIViewController {
+
+    @IBOutlet weak var backdropImageView: UIImageView!
+    @IBOutlet weak var posterImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    var movie: [String: Any]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        titleLabel.text = movie["title"] as? String
+        titleLabel.sizeToFit()
+        synopsisLabel.text = movie["overview"] as? String
+        synopsisLabel.sizeToFit()
+        
+        let backdropPath = movie["backdrop_path"] as! String
+        let backdropUrl = URL(string: "https://image.tmdb.org/t/p/w780" + backdropPath)
+        backdropImageView.af_setImage(withURL: backdropUrl!)
+        
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseUrl + posterPath)
+        posterImageView.af_setImage(withURL: posterUrl!)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
